e4924294a1b22deed7fc51d7d12b4b77d591ba06
Aderson Bertim <adersonbertim@gmail.com>
2022-04-05 00:46:18 -0300

    Scripts em C

---
+++ Scripts/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// Import libraries and files external and global definitions of elements.
+
+// A Simple form to say Hello World :)
+int main() {
+  printf("Hello World\n");
+
+  return 0;
+}
